f4a728b4a3eaa03fb923a0d255ce904ef765668c
BeyJohn <Beyjohn1@gmail.com>
2017-05-10 15:25:08 -0500

    First Commit

---
+++ Poker/Card.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Sulimn
+{
+	/// <summary>Represents a playing card.</summary>
+	internal class Card : INotifyPropertyChanged, IEquatable<Card>
+	{
+		private string _name;
+		private CardSuit _suit;
+		private int _value;
+		private bool _hidden;
+
+		/// <summary>Represents the suit of a playing card.</summary>
+		internal enum CardSuit { Spades, Hearts, Clubs, Diamonds }
+
+		#region Properties
+
+		/// <summary>The name of the card.</summary>
+		public string Name
+		{
+			get => _name;
+			set
+			{
+				_name = value;
+				OnPropertyChanged("Name");
+			}
+		}
+
+		/// <summary>The suit of the card.</summary>
+		public CardSuit Suit
+		{
+			get => _suit;
+			set
+			{
+				_suit = value;
+				OnPropertyChanged("CardSuit");
+			}
+		}
+
+		/// <summary>The value of the card.</summary>
+		public int Value
+		{
+			get => _value;
+			set
+			{
+				_value = value;
+				OnPropertyChanged("Value");
+			}
+		}
+
+		/// <summary>Should the Card be hidden from the player?</summary>
+		public bool Hidden
+		{
+			get => _hidden;
+			set
+			{
+				_hidden = value;
+				OnPropertyChanged("Hidden");
+			}
+		}
+
+		/// <summary>Returns the name and suit of the card.</summary>
+		public string CardToString => Hidden ? "?? of ??" : $"{Name} of {Suit}";
+
+		#endregion Properties
+
+		#region Data-Binding
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void OnPropertyChanged(string property)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+		}
+
+		#endregion Data-Binding
+
+		#region Override Operators
+
+		private static bool Equals(Card left, Card right)
+		{
+			if (ReferenceEquals(null, left) && ReferenceEquals(null, right)) return true;
+			if (ReferenceEquals(null, left) ^ ReferenceEquals(null, right)) return false;
+			return string.Equals(left.Name, right.Name, StringComparison.OrdinalIgnoreCase) && left.Suit == right.Suit &&
+			 left.Value == right.Value && left.Hidden == right.Hidden;
+		}
+
+		public sealed override bool Equals(object obj)
+		{
+			return Equals(this, obj as Card);
+		}
+
+		public bool Equals(Card otherCard)
+		{
+			return Equals(this, otherCard);
+		}
+
+		public static bool operator ==(Card left, Card right)
+		{
+			return Equals(left, right);
+		}
+
+		public static bool operator !=(Card left, Card right)
+		{
+			return !Equals(left, right);
+		}
+
+		public sealed override int GetHashCode()
+		{
+			return base.GetHashCode() ^ 17;
+		}
+
+		public sealed override string ToString()
+		{
+			return CardToString;
+		}
+
+		#endregion Override Operators
+
+		#region Constructors
+
+		/// <summary>Initializes a default instance of Card.</summary>
+		internal Card()
+		{
+		}
+
+		/// <summary>Initializes an instance of Card by assigning Properties.</summary>
+		/// <param name="name">Name of Card</param>
+		/// <param name="suit">Suit of Card</param>
+		/// <param name="value">Value of Card</param>
+		/// <param name="hidden">Should the Card be hidden from the player?</param>
+		internal Card(string name, CardSuit suit, int value, bool hidden)
+		{
+			Name = name;
+			Suit = suit;
+			Value = value;
+			Hidden = hidden;
+		}
+
+		/// <summary>Replaces this instance of Card with another instance.</summary>
+		/// <param name="otherCard">Instance to replace this instance</param>
+		internal Card(Card otherCard)
+		{
+			Name = otherCard.Name;
+			Suit = otherCard.Suit;
+			Value = otherCard.Value;
+			Hidden = otherCard.Hidden;
+		}
+
+		/// <summary>Replaces this instance of Card with another instance.</summary>
+		/// <param name="otherCard">Instance to replace this instance</param>
+		/// <param name="hidden">Should the Card be hidden from the player?</param>
+		internal Card(Card otherCard, bool hidden)
+		{
+			Name = otherCard.Name;
+			Suit = otherCard.Suit;
+			Value = otherCard.Value;
+			Hidden = hidden;
+		}
+
+		#endregion Constructors
+	}
+}
--- Roulette/MainWindow.xaml.cs
+++ Roulette/MainWindow.xaml.cs
@@ -1,28 +1,155 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Roulette
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
+
 	public partial class MainWindow : Window
 	{
+		private int _MoneyLeft;
+		private int _CurrentBet;
+		private RadioButton _CurrentRadioButton;
+
+		private List<int> _Row1, _Row2, _Row3, _Blacks, _Reds;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			_MoneyLeft = 1000;
+			RemainingMoney.Text = _MoneyLeft + "";
+			_Row1 = new List<int>();
+			_Row2 = new List<int>();
+			_Row3 = new List<int>();
+			for (int o = 0; o < 12; o++)
+			{
+				_Row1.Add(o * 3 + 1);
+				_Row2.Add(o * 3 + 2);
+				_Row3.Add(o * 3 + 2);
+			}
+
+			_Reds = new List<int>
+			{
+				1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36
+			};
+			_Blacks = new List<int>
+			{
+				2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35
+			};
+		}
+
+		private void Spin(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				_CurrentBet = int.Parse(Bet.Text);
+				if (_CurrentBet > _MoneyLeft)
+				{
+					DisplayText.Text += "\nYour bet cannot be greater than the money you have left!";
+					return;
+				}
+				else if (_CurrentBet == 0)
+				{
+					DisplayText.Text += "\nYour bet cannot be 0!";
+					return;
+				}
+			}
+			catch (Exception E)
+			{
+				DisplayText.Text += "\nChoose your space(s), and place your bet.";
+				return;
+			}
+			int multiplier = GetMult();
+
+			int r = new Random().Next(38);
+			switch (r)
+			{
+				case 0: // 0
+					DisplayText.Text += "\nSpun a 0.";
+					if ((string)_CurrentRadioButton.Content == "0")
+					{
+						_MoneyLeft += _CurrentBet * multiplier;
+					}
+					else
+					{
+						_MoneyLeft -= _CurrentBet;
+					}
+					break;
+
+				case 37: // 00
+					DisplayText.Text += "\nSpun a 00.";
+					if ((string)_CurrentRadioButton.Content == "00")
+					{
+						_MoneyLeft += _CurrentBet * multiplier;
+					}
+					else
+					{
+						_MoneyLeft -= _CurrentBet;
+					}
+					break;
+
+				default:
+					DisplayText.Text += "\nSpun a " + r + ".";
+					if ((string)_CurrentRadioButton.Content == ("" + r) || ((string)_CurrentRadioButton.Content == "Row1" && _Row1.Contains(r)) || ((string)_CurrentRadioButton.Content == "Row2" && _Row2.Contains(r)) || ((string)_CurrentRadioButton.Content == "Row3" && _Row3.Contains(r)) || ((string)_CurrentRadioButton.Content == "Evens" && r % 2 == 0) || ((string)_CurrentRadioButton.Content == "Odds" && r % 2 == 1) || ((string)_CurrentRadioButton.Content == "Blacks" && _Blacks.Contains(r)) || ((string)_CurrentRadioButton.Content == "Reds" && _Reds.Contains(r)) || ((string)_CurrentRadioButton.Content == "1st 12" && r < 13 && r > 0) || ((string)_CurrentRadioButton.Content == "2nd 12" && r < 25 && r > 12) || ((string)_CurrentRadioButton.Content == "3rd 12" && r < 37 && r > 24) || ((string)_CurrentRadioButton.Content == "1-18" && r > 0 && r < 19) || ((string)_CurrentRadioButton.Content == "19-36" && r > 18 && r < 37))
+					{
+						_MoneyLeft += _CurrentBet * multiplier;
+					}
+					else
+					{
+						_MoneyLeft -= _CurrentBet;
+					}
+					break;
+			}
+			RemainingMoney.Text = _MoneyLeft + "";
+		}
+
+		private int GetMult()
+		{
+			switch (_CurrentRadioButton.Content)
+			{
+				case "Row 1":
+				case "Row 2":
+				case "Row 3":
+				case "1st 12":
+				case "2nd 12":
+				case "3rd 12":
+					return 4;
+
+				case "Evens":
+				case "Odds":
+				case "Reds":
+				case "Blacks":
+				case "1-18":
+				case "18-36":
+					return 3;
+
+				default:
+					return 37;
+			}
+		}
+
+		private void AllCheckBoxes_CheckedChanged(object sender, EventArgs e)
+		{
+			if ((bool)((RadioButton)sender).IsChecked)
+			{
+				_CurrentRadioButton = (RadioButton)sender;
+			}
+			// DisplayText.Text += "\n" + _CurrentRadioButton.Content;
+		}
+
+		private void DisplayText_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			DisplayText.ScrollToEnd();
+		}
+
+		private void Bet_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			if (Bet.Text != "" && !char.IsNumber(Bet.Text[Bet.Text.Length - 1]))
+			{
+				Bet.Text = Bet.Text.Substring(0, Bet.Text.Length - 1);
+			}
+			Bet.CaretIndex = Bet.Text.Length;
 		}
 	}
 }
